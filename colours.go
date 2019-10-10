2e8f1c70b57a92c2e90e9863701edc30e2e22f76
Tom McKeesick <tmck01@gmail.com>
2019-10-10 18:22:11 +1100

    Update colour codes with variants

--- colours.go
+++ colours.go
@@ -6,11 +6,23 @@ import (
 )
 
 func CPrintln(c Colour.Colour, s string) {
-	fmt.Printf("\033%s%s\033[0m\n", c.Fmt, s)
+	fmt.Printf("%-15s \033%s%s\033[0m\n", c.Name, c.Fmt, s)
+}
+
+func CPrintVariantln(c Colour.Colour, variant string, s string) {
+	// fmt.Println(c.Name, variant, c.Fmt, s)
+	fmt.Printf("%-15s %s \033[%s;%sm %s \033[0m\n", c.Name, variant, variant, c.Fmt, s)
 }
 
 func main() {
 	for _, c := range Colour.All {
-		CPrintln(c, "hello world")
+		// CPrintln(c, "hello world")
+		CPrintVariantln(c, "0", "hello world")
+		CPrintVariantln(c, "1", "hello world")
+		CPrintVariantln(c, "2", "hello world")
+		CPrintVariantln(c, "3", "hello world")
+		CPrintVariantln(c, "4", "hello world")
+		CPrintVariantln(c, "7", "hello world")
+		CPrintVariantln(c, "9", "hello world")
 	}
 }
--- colours/codes.go
+++ colours/codes.go
@@ -8,24 +8,15 @@ type Colour struct {
 }
 
 var (
-	Red         = Colour{Name: "Red", Fmt: "[0;31m"}
-	Black       = Colour{Name: "Black", Fmt: "[0;30m"}
-	Green       = Colour{Name: "Green", Fmt: "[0;32m"}
-	Brown       = Colour{Name: "Brown", Fmt: "[0;33m"}
-	Blue        = Colour{Name: "Blue", Fmt: "[0;34m"}
-	Purple      = Colour{Name: "Purple", Fmt: "[0;35m"}
-	Cyan        = Colour{Name: "Cyan", Fmt: "[0;36m"}
-	Gray        = Colour{Name: "Gray", Fmt: "[0;37m"}
-	DarkGray    = Colour{Name: "DarkGray", Fmt: "[1;30m"}
-	LightRed    = Colour{Name: "LightRed", Fmt: "[1;31m"}
-	LightGreen  = Colour{Name: "LightGreen", Fmt: "[1;32m"}
-	Yellow      = Colour{Name: "Yellow", Fmt: "[1;33m"}
-	LightBlue   = Colour{Name: "LightBlue", Fmt: "[1;34m"}
-	LightPurple = Colour{Name: "LightPurple", Fmt: "[1;35m"}
-	LightCyan   = Colour{Name: "LightCyan", Fmt: "[1;36m"}
-	White       = Colour{Name: "White", Fmt: "[1;37m"}
-	All         = []Colour{
-
+	Black  = Colour{Fmt: "30", Name: "Black"}
+	Red    = Colour{Fmt: "31", Name: "Red"}
+	Green  = Colour{Fmt: "32", Name: "Green"}
+	Brown  = Colour{Fmt: "33", Name: "Brown"}
+	Blue   = Colour{Fmt: "34", Name: "Blue"}
+	Purple = Colour{Fmt: "35", Name: "Purple"}
+	Cyan   = Colour{Fmt: "36", Name: "Cyan"}
+	Gray   = Colour{Fmt: "37", Name: "Gray"}
+	All    = []Colour{
 		Red,
 		Black,
 		Green,
@@ -34,13 +25,5 @@ var (
 		Purple,
 		Cyan,
 		Gray,
-		DarkGray,
-		LightRed,
-		LightGreen,
-		Yellow,
-		LightBlue,
-		LightPurple,
-		LightCyan,
-		White,
 	}
 )
